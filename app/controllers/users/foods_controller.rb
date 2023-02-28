53cdefac5b5c98e578730bc30988d2b0a35ce3bc
omusubitabetai <ec2-user@ip-172-31-1-90.ap-northeast-1.compute.internal>
2023-02-28 14:21:17 +0000

    退会機能の実装

--- app/controllers/users/foods_controller.rb
+++ app/controllers/users/foods_controller.rb
@@ -1,2 +1,28 @@
 class Users::FoodsController < ApplicationController
+
+  def new
+    @food = Food.new
+  end
+
+  def create
+    food = Food.new(food_params)
+    food.save
+    redirect_to foods_path
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  private
+
+  def food_params
+    params.require(:food).permit(:title, :text)
+  end
+
 end
